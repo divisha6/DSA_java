d79b622288fc6ed1ad81c532e56dc5fdf5bfbed1
Divisha Gupta <2020.divisha.gupta@ves.ac.in>
2022-10-06 23:36:06 +0530

    selection sort done

---
+++ Sorting_Algorithms/SelectionSort.java
@@ -0,0 +1,15 @@
+package Sorting_Algorithms;
+
+public class SelectionSort {
+    public static void main(String[] args) {
+        
+    }
+
+    public static void selection(int[] arr){
+        for (int i = 0; i < arr.length; i++) {
+            // find the biggest element in the given array and swap with the correct index
+            int last= arr.length - i-1;
+            
+        }
+    }
+}
