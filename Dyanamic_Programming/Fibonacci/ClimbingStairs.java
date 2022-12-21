0be394dd259930f58685517302f9046aa33b6277
2020.divisha.gupta@ves.ac.in <2020.divisha.gupta@ves.ac.in>
2022-12-21 23:36:49 +0530

    started 0-1 knapsack

--- Dyanamic_Programming/Fibonacci/ClimbingStairs.java
+++ Dyanamic_Programming/Fibonacci/ClimbingStairs.java
@@ -5,11 +5,14 @@ import java.util.Arrays;
 public class ClimbingStairs {
     public static void main(String[] args) {
         int n= 4;
-        int[] waysArr= new int[n+1];
-        Arrays.fill(waysArr, -1);
-        System.out.println(countWays(n, waysArr));
+        // int[] waysArr= new int[n+1];
+        // Arrays.fill(waysArr, -1);
+        // System.out.println(countWays(n, waysArr));
+        System.out.println(numberOfWays(n));
 
     }
+
+    // MEMOIZATION
     public static int countWays(int n, int[] waysArr) {
         if(n==0 || n==1){
             return 1;
@@ -25,5 +28,18 @@ public class ClimbingStairs {
         waysArr[n]= countWays(n-1, waysArr) + countWays(n-2, waysArr);
         
         return waysArr[n];
+    }
+
+    // TABULATION
+    public static int numberOfWays(int n){
+        int[] newArr= new int[n+1];
+        newArr[0] = 0;
+        newArr[1]=1;
+        newArr[2]=2;
+
+        for (int i = 3; i < newArr.length; i++) {
+            newArr[i]= newArr[i-1]+ newArr[i-2];
+        }
+        return newArr[n];
     }
 }
