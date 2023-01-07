5892ff6fcdc3878bc6e1cddc1268579bac5a6efd
2020.divisha.gupta@ves.ac.in <2020.divisha.gupta@ves.ac.in>
2023-01-07 14:58:14 +0530

    added prob 33

---
+++ Leetcode/Prob33.java
@@ -0,0 +1,12 @@
+public class Prob33 {
+    public int search(int[] nums, int target) {
+        int result = -1;
+
+        for(int i=0; i< nums.length; i++){
+            if(nums[i]==target){
+                result = i;
+            }
+        }
+        return result;
+    }
+}
