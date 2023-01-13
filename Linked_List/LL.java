75087a9ee93c98e19170302cd4ae90240dee83fc
2020.divisha.gupta@ves.ac.in <2020.divisha.gupta@ves.ac.in>
2023-01-13 21:59:29 +0530

    reverse LL done

--- Linked_List/LL.java
+++ Linked_List/LL.java
@@ -97,6 +97,25 @@ public class LL {
         System.out.print("null");
     }
 
+    // reversing linked list
+    public void reverseList() {
+        if(head == null || head.next == null) {
+            return;
+        }
+  
+        Node prevNode = head;
+        Node currNode = head.next;
+        while(currNode != null) {
+            Node nextNode = currNode.next;
+            currNode.next = prevNode;
+            prevNode = currNode;
+            currNode = nextNode;
+        }
+        head.next = null;
+        head = prevNode;
+    }
+ 
+
     public static void main(String[] args) {
         LL list = new LL();
         list.addFirst("is");
@@ -106,8 +125,7 @@ public class LL {
         list.printingLL();
         System.out.println();
 
-        list.deleteFirst();
-        list.deleteLast();
+        list.reverseList();
         list.printingLL();
         System.out.println();
 
---
+++ Linked_List/LL2.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class LL2{
+    public static void main(String[] args) {
+        LinkedList<String> list = new LinkedList<String>();
+
+        list.addFirst("is");
+        list.addFirst("name");
+
+        list.remove(0);
+    }
+}
