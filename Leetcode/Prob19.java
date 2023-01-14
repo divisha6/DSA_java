24aa7a6bda2d14df803872be8ad33c7e719d095b
2020.divisha.gupta@ves.ac.in <2020.divisha.gupta@ves.ac.in>
2023-01-14 14:57:24 +0530

    added prob 19

---
+++ Leetcode/Prob19.java
@@ -0,0 +1,33 @@
+public class Prob19 {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        if(head.next==null){
+            return null;
+        }
+
+           // size
+        int size=0;
+        ListNode curr = head;
+        while(curr != null){
+            curr = curr.next;
+            size++;
+        }
+
+        if(n==size){
+            return head.next;
+        }
+
+       // previous node to the target node so that we can delete the next node of the previous node
+        int indexToSearch= size-n;
+        ListNode prev=head;
+        int i=1;
+       while(i<indexToSearch){
+           prev= prev.next;
+           i++;
+       }
+
+       // actual deletion
+       prev.next = prev.next.next;
+       return head;
+
+   }
+}
