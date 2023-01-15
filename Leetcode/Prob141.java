0db04030357d8c2426bef0b9839f7b9f5bc0ff2a
2020.divisha.gupta@ves.ac.in <2020.divisha.gupta@ves.ac.in>
2023-01-15 12:34:32 +0530

    added prob 141

---
+++ Leetcode/Prob141.java
@@ -0,0 +1,20 @@
+public class Prob141{
+    public boolean hasCycle(ListNode head) {
+        if(head == null){
+            return false;
+        }
+
+        ListNode hare= head;
+        ListNode turtle = head;
+
+        while(hare != null && hare.next != null){
+            hare = hare.next.next;
+            turtle=turtle.next;
+
+            if(hare==turtle){
+                return true;
+            }
+        }
+        return false;
+    }
+}
