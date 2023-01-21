d5fd105f37283da1d995d5d6cea90f43c9303a56
2020.divisha.gupta@ves.ac.in <2020.divisha.gupta@ves.ac.in>
2023-01-21 20:32:01 +0530

    added queue using ArrayDequeue

--- Data Structures/QueueArrDeq.java
+++ Data Structures/QueueArrDeq.java
@@ -1,9 +1,9 @@
 import java.util.*;
 
-public class QueueJCF {
+public class QueueArrDeq {
     public static void main(String[] args) {
-        Queue<Integer> q = new LinkedList<>();
-        // this is done because Queue is an interface and not a class in java
+        Queue<Integer> q= new ArrayDeque<>();
+        // this is done because Queue is an interface and not a class in java 
         q.add(1);
         q.add(2);
         q.add(3);
@@ -11,6 +11,6 @@ public class QueueJCF {
         while(!q.isEmpty()){
             System.out.println(q.peek());
             q.remove();
+        }
     }
-}
 }
