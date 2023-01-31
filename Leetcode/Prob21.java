9eb44d091aeaa236590f7c7a4772a150fc74a9f1
2020.divisha.gupta@ves.ac.in <2020.divisha.gupta@ves.ac.in>
2023-01-31 23:52:26 +0530

    added prob 21 - LL

---
+++ Leetcode/Prob21.java
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+public class Prob21 {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode new1= new ListNode();
+        ListNode new2= new ListNode();
+
+        if(list1 != null && list2 != null){
+            if(list1.val > list2.val){
+                list2.next = mergeTwoLists(list1, list2.next);
+                return list2;
+            } else{
+                list1.next = mergeTwoLists(list1.next, list2);
+                return list1;
+            }
+        }
+        if(list1==null){
+            return list2;
+        }
+        return list1;
+    }
+}
+
+
+
+   
