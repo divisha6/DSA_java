c8ec7e66129b05c32f87dafffafce52f4c033d7b
2020.divisha.gupta@ves.ac.in <2020.divisha.gupta@ves.ac.in>
2023-02-13 20:30:39 +0530

    added Prob 1304

---
+++ Leetcode/Prob1304.java
@@ -0,0 +1,25 @@
+public class Prob1304 {
+    public int[] sumZero(int n) {
+        int[] ans = new int[n];
+        if(n==1 || n==0){
+            return ans;
+        }
+
+        if(n%2==1){
+            ans[n/2+1]=0;
+        }       
+
+        int i=0;
+        int j=n-1;
+        int count =1;
+        while(i!=n/2){
+            ans[i]=count;
+            ans[j]= count*-1;
+            i++;
+            j--;
+            count++;
+        }
+
+        return ans;
+    }
+}
