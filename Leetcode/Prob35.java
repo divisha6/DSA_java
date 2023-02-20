192d247a405661b2d469e2bbc75c443e38d0126a
2020.divisha.gupta@ves.ac.in <2020.divisha.gupta@ves.ac.in>
2023-02-20 23:53:28 +0530

    added prob 35

---
+++ Leetcode/Prob35.java
@@ -0,0 +1,21 @@
+public class Prob35 {
+    public int searchInsert(int[] nums, int target) {
+        int end = nums.length -1;
+        int start = 0;
+        while(start<=end){
+            int mid = start + (end-start)/2;
+            if(nums[mid]==target){
+                return mid;
+            }
+
+            if(nums[mid]>target){
+                end = mid -1;
+            }
+
+            else{
+                start = mid+1;
+            }
+        }
+        return end +1;
+    }
+}
