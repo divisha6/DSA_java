43eeb86f8f1f83d01bed49a382e75d6aeddce994
2020.divisha.gupta@ves.ac.in <2020.divisha.gupta@ves.ac.in>
2023-02-27 11:51:39 +0530

    added prob 724

---
+++ Leetcode/Prob724.java
@@ -0,0 +1,22 @@
+public class Prob724 {
+    public int pivotIndex(int[] nums) {
+        int[] leftSum = new int[nums.length];
+        int[] rightSum = new int[nums.length];
+        leftSum[0]=nums[0];
+        for(int i=1; i<nums.length ; i++){
+            leftSum[i] = leftSum[i-1] + nums[i];
+        }
+        
+        rightSum[nums.length-1]=nums[nums.length-1];
+        for(int i=nums.length -2; i>=0 ; i--){
+            rightSum[i] = rightSum[i+1] + nums[i];
+        }
+
+        for(int i =0; i<nums.length ; i++){
+            if(rightSum[i]==leftSum[i]){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
