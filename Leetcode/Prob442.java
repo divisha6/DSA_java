d659f3c6455fdb3b3bf8f0a1e901c7ad0bb4f2c9
2020.divisha.gupta@ves.ac.in <2020.divisha.gupta@ves.ac.in>
2023-03-10 22:42:03 +0530

    added prob 442 and 2176

---
+++ Leetcode/Prob442.java
@@ -0,0 +1,15 @@
+public class Prob442{
+    public List<Integer> findDuplicates(int[] nums) {
+        // ArrayList<Integer> list = new ArrayList<>();
+        HashSet<Integer> set = new HashSet<>();
+        ArrayList<Integer> ans = new ArrayList<>();
+            for(int i=0; i<nums.length; i++){
+                if(!set.contains(nums[i])){
+                    set.add(nums[i]);
+                } else{
+                    ans.add(nums[i]);
+                }
+            }
+        return ans;
+    }
+}
