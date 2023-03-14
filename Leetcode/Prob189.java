7177754edd4f88adea3afbb6ab1fd11bbe80b1c6
2020.divisha.gupta@ves.ac.in <2020.divisha.gupta@ves.ac.in>
2023-03-14 11:34:34 +0530

    added prob 189 with new soln

--- Leetcode/Prob189.java
+++ Leetcode/Prob189.java
@@ -1,23 +1,35 @@
 public class Prob189{
     public int[] rotate(int[] arr, int k) {
-        int len = arr.length-1;
-        k=k%(len+1);
-        arr = rev(arr,0,len-k);
-        arr=rev(arr,len-k+1,len);
-        arr=rev(arr,0,len);
+        int n= arr.length;
+        k=k%n;
+        int[] nums = new int[k];
+
+        // saving nums after k
+        int a=0;
+        for(int i=n-k; i<n; i++){
+            nums[a]=arr[i];
+            a++;
+        }
+        
+
+        int[] wow = new int[n-k];
+        int j=0;
+        for(int i=0; i<n-k; i++){
+            wow[j]=arr[i];
+            j++;
+        }
+        
+        int m=0;
+        for(int i=0; i<k; i++){
+            arr[i]=nums[m];
+            m++;
+        }
+
+        for(int i=0; i<n-k; i++){
+            arr[m]=wow[i];
+            m++;
+        }
+
         return arr;
     }
-
-   public int[] rev(int[] arr,int low,int hi)
-    {
-        while(low<hi)
-        {
-           int temp=arr[low];
-           arr[low]=arr[hi];
-           arr[hi]=temp;
-           hi--;
-           low++;
-        }
-        return arr;
-}
 }
