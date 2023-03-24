579d3b96227d42e02ddda6583c2534b8fb0d84e2
2020.divisha.gupta@ves.ac.in <2020.divisha.gupta@ves.ac.in>
2023-03-24 20:10:51 +0530

    added prob 47

---
+++ Leetcode/Prob47.java
@@ -0,0 +1,35 @@
+public class Prob47 {
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        HashSet<List<Integer>> ans = new HashSet<>();
+        recurPermute(0, nums, ans);
+        List<List<Integer>> arrayList = new ArrayList<>();
+        arrayList.addAll(ans);
+        return arrayList;
+    }
+
+    private void recurPermute(int index, int[] nums, HashSet<List<Integer>> ans){
+        Set<Integer> set = new HashSet<>();
+        if(index==nums.length){
+            List<Integer> ds = new ArrayList<>();
+            for(int i=0; i<nums.length; i++){
+                ds.add(nums[i]);
+            }
+            // if(!ans.contains(new ArrayList<>(ds)))
+            // {ans.add(ds);}
+            ans.add(new ArrayList<>(ds));
+            return ;
+        }
+
+        for(int i=index; i<nums.length; i++){
+                swap(i, index, nums);
+                recurPermute(index+1, nums, ans);
+                swap(i, index, nums);
+        }
+    }
+
+    private void swap(int a, int b, int[] nums){
+        int t = nums[a];
+        nums[a]= nums[b];
+        nums[b]=t;
+    }
+}
