3cd267f367d1c1fb712eb16df77616895d83c327
2020.divisha.gupta@ves.ac.in <2020.divisha.gupta@ves.ac.in>
2023-03-31 01:44:31 +0530

    added prob 744

---
+++ Leetcode/Prob744.java
@@ -0,0 +1,15 @@
+public class Prob744 {
+    public char nextGreatestLetter(char[] letters, char target) {
+        ArrayList<Character> list = new ArrayList<>();
+        for(int i=0; i<letters.length; i++){
+            list.add(letters[i]);
+        }
+
+        list.add(target);
+        Collections.sort(list);
+        if(list.get(list.size()-1)==target) return list.get(0);
+        
+        
+        return list.get(list.lastIndexOf(target)+1);
+    }
+}
