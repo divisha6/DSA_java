4c0c8fa495cb5429318a77ffd45376313bf91f0c
2020.divisha.gupta@ves.ac.in <2020.divisha.gupta@ves.ac.in>
2023-03-30 19:36:01 +0530

    added prob 83

---
+++ Leetcode/Prob83.java
@@ -0,0 +1,14 @@
+public class Prob83 {
+    public ListNode deleteDuplicates(ListNode head) {
+        if(head==null || head.next==null) return head;
+        ListNode curr = head;
+        while(curr!=null && curr.next != null){
+            if(curr.next.val==curr.val){
+                curr.next = curr.next.next;
+            } else{
+                curr = curr.next;
+            }
+        }
+        return head;
+    }
+}
