b1137c4c66a66199cee9b3e2454d8eb17fc82728
2020.divisha.gupta@ves.ac.in <2020.divisha.gupta@ves.ac.in>
2023-04-07 15:22:19 +0530

    added hackerrank prob greedy florist

---
+++ Hackerrank/GreedyFlorist.java
@@ -0,0 +1,21 @@
+package Hackerrank;
+import java.util.*;
+
+public class GreedyFlorist {
+    static int getMinimumCost(int k, int[] c) {
+        int sum=0;
+        int round=0;
+        int var = k;
+        Arrays.sort(c);
+        for(int i=c.length-1; i>=0; i--){
+                sum += (round+1)*c[i];
+                var--;
+                
+            if(var==0){
+                round++;
+                var=k;
+            }
+        }
+        return sum;
+    }
+}
