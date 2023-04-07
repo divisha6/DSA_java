436d503acdc4420785e83cf8c6562b9f640e2035
2020.divisha.gupta@ves.ac.in <2020.divisha.gupta@ves.ac.in>
2023-04-07 18:43:24 +0530

    added maxmin- hackerrank

---
+++ Hackerrank/MaxMin.java
@@ -0,0 +1,16 @@
+package Hackerrank;
+import java.util.*;
+
+public class MaxMin {
+    public static int maxMin(int k, List<Integer> arr) {
+        // Write your code here
+            long min=Integer.MAX_VALUE;
+            long diff=0;
+            Collections.sort(arr);
+            for(int i=0; i<arr.size()-k+1; i++){
+                diff= arr.get(i+k-1)-arr.get(i);
+                min = Math.min(min, diff);
+            }
+            return (int)min;
+        }
+}
