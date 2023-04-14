b5c007efd35355ba730ca95d27ad9260577b8b7d
2020.divisha.gupta@ves.ac.in <2020.divisha.gupta@ves.ac.in>
2023-04-14 13:37:26 +0530

    solved 1137 tribonacci using DP

---
+++ Leetcode/Prob1137.java
@@ -0,0 +1,12 @@
+public class Prob1137 {
+    public int tribonacci(int n){
+        int[] tri = new int[n+3];
+        tri[0]=0;
+        tri[1]=1;
+        tri[2]=1;
+        for(int i=3; i<=n ; i++){
+            tri[i]= tri[i-1]+ tri[i-2]+ tri[i-3];
+        }
+        return tri[n];
+    }
+}
