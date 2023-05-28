72800911644a9fa567f367c99f4f42b8b6c97d67
2020.divisha.gupta@ves.ac.in <2020.divisha.gupta@ves.ac.in>
2023-05-28 22:42:15 +0530

    added prob 2706

---
+++ Leetcode/Prob2706.java
@@ -0,0 +1,9 @@
+public class Prob2706 {
+    public int buyChoco(int[] prices, int money) {
+        Arrays.sort(prices);
+        int sum= prices[0] + prices[1];
+        if(sum>money) return money;
+
+        return money-sum;
+    }
+}
