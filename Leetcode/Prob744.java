bdf861967299e7593c30bde4e46a43c706cd796d
2020.divisha.gupta@ves.ac.in <2020.divisha.gupta@ves.ac.in>
2023-06-09 15:26:02 +0530

    added optimised sol for prob 744

--- Leetcode/Prob744.java
+++ Leetcode/Prob744.java
@@ -2,16 +2,23 @@ import java.util.*;
 
 public class Prob744 {
     public char nextGreatestLetter(char[] letters, char target) {
-        ArrayList<Character> list = new ArrayList<>();
+        
         for(int i=0; i<letters.length; i++){
-            list.add(letters[i]);
+            char c = letters[i];
+            if(c-target>0) return c;
         }
+        return letters[0];
 
-        list.add(target);
-        Collections.sort(list);
-        if(list.get(list.size()-1)==target) return list.get(0);
+        // ArrayList<Character> list = new ArrayList<>();
+        // for(int i=0; i<letters.length; i++){
+        //     list.add(letters[i]);
+        // }
+
+        // list.add(target);
+        // Collections.sort(list);
+        // if(list.get(list.size()-1)==target) return list.get(0);
         
         
-        return list.get(list.lastIndexOf(target)+1);
+        // return list.get(list.lastIndexOf(target)+1);
     }
 }
