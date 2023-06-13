238fe1280c4b9b32ece82377cb3a2c24cd6a1e78
2020.divisha.gupta@ves.ac.in <2020.divisha.gupta@ves.ac.in>
2023-06-13 13:48:03 +0530

    added prob 342

---
+++ Leetcode/Prob342.java
@@ -0,0 +1,10 @@
+public class Prob342 {
+    public boolean isPowerOfFour(int n) {
+        if(n==0) return false;
+        if(n==1) return true;
+
+        if(n%4==0) return isPowerOfFour(n/4);
+
+        return false;
+    }
+}
