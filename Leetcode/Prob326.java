153f1fbf5bfe41f740287ddc7772ee73c2dcebfc
2020.divisha.gupta@ves.ac.in <2020.divisha.gupta@ves.ac.in>
2023-06-13 13:46:01 +0530

    added prob 326

---
+++ Leetcode/Prob326.java
@@ -0,0 +1,10 @@
+public class Prob326 {
+    public boolean isPowerOfThree(int n) {
+        if(n==0) return false;
+        if(n==1) return true;
+
+        if(n%3==0) return isPowerOfThree(n/3);
+
+        return false;
+    }
+}
