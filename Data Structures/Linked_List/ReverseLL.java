a5ed2f3eef1b16fd1fafe3c4effd01f0549bfa06
2020.divisha.gupta@ves.ac.in <2020.divisha.gupta@ves.ac.in>
2023-06-29 12:14:50 +0530

    corrected erroneous files

--- Data Structures/Linked_List/ReverseLL.java
+++ Data Structures/Linked_List/ReverseLL.java
@@ -1,6 +1,18 @@
 import java.util.*;
 
 public class ReverseLL {
+    private int size;
+    public class Node{
+        String data;
+        Node next;
+
+        Node(String data){
+            this.data = data;
+            this.next = null;
+            size++;
+        }
+    }
+
     Node head;
     public static void main(String[] args) {
         LinkedList<Integer> list = new LinkedList<>();
