870603608665f94687d3f7bfe47751380e8b1ed1
2020.divisha.gupta@ves.ac.in <2020.divisha.gupta@ves.ac.in>
2023-07-09 22:17:20 +0530

    min path sum- DP

---
+++ Leetcode/Prob64.java
@@ -0,0 +1,21 @@
+public class Prob64 {
+    public int minPathSum(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0 && j == 0)
+                    continue;
+                if (i == 0)
+                    grid[i][j] += grid[i][j - 1];
+                // topmost row ^
+                else if (j == 0)
+                    grid[i][j] += grid[i - 1][j];
+                // leftmost col ^
+                else
+                    grid[i][j] += Math.min(grid[i - 1][j], grid[i][j - 1]);
+            }
+        }
+        return grid[m - 1][n - 1];
+    }
+}
