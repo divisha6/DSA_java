69c9b035b63aca930fcccab42b3f26ebaf9e4ab9
2020.divisha.gupta@ves.ac.in <2020.divisha.gupta@ves.ac.in>
2023-07-23 23:41:42 +0530

    rotate string sum- easy logic

---
+++ Leetcode/Prob796.java
@@ -0,0 +1,11 @@
+public class Prob796 {
+    public boolean rotateString(String s, String goal) {
+        if (s.length() != goal.length())
+            return false;
+        String check = s + s;
+        if (check.contains(goal)) {
+            return true;
+        }
+        return false;
+    }
+}
