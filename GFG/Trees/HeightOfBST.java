e27d4604b235735202a49a0b291eac611224469f
2020.divisha.gupta@ves.ac.in <2020.divisha.gupta@ves.ac.in>
2023-08-05 21:10:06 +0530

    height of BST

---
+++ GFG/Trees/HeightOfBST.java
@@ -0,0 +1,12 @@
+package Trees;
+
+public class HeightOfBST {
+    int height(Node node) 
+    {
+        // code here 
+        if(node==null) return 0;
+        int lh = height(node.left);
+        int rh = height(node.right);
+        return Math.max(lh,rh)+1;
+    }
+}
