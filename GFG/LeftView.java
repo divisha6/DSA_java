a0805d93e57e0da871f12c6d608f694753fb263c
2020.divisha.gupta@ves.ac.in <2020.divisha.gupta@ves.ac.in>
2023-08-05 19:33:36 +0530

    left view of a binary tree added

---
+++ GFG/LeftView.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+
+// /A Binary Tree node
+class Node
+{
+    int data;
+    Node left, right;
+
+    Node(int item)
+    {
+        data = item;
+        left = right = null;
+    }
+}
+
+// left view of binary tree ******
+public class LeftView {
+    ArrayList<Integer> leftView(Node root)
+    {
+      // Your code here
+      ArrayList<Integer> ans = new ArrayList<>();
+      helper(root, ans, 0);
+      return ans;
+    }
+    
+    void helper(Node root, ArrayList<Integer> ans, int level){
+        // ans.add(root.data);
+        if(root == null) return ;
+        if(ans.size()<=level) ans.add(level, root.data);
+        helper(root.left, ans, level+1);
+        helper(root.right, ans, level+1);
+    }
+}
